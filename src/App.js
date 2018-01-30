74ade21bca02a9c01379303337db8176f3de1814
Jon Middleton <middleton.jd@gmail.com>
2018-01-29 21:30:22 -0800

    Set up toto pull info from Spotify API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import queryString from 'query-string';
 
-let defaultTextColor = '#fff';
 let defaultStyle = {
-  color: defaultTextColor
+  color: '#fff'
 };
+
 let fakeServerData = {
   user: {
     name: 'Jon',
     playlists: [
-      {
-        name: 'My favorites',
-        songs: [{name:'Song 1', duration: 1345}, {name:'Song 2', duration: 1236}, {name:'Song 3', duration: 1405}]
-      },
       {
         name: 'Dance Mix',
         songs: [{name:'Freeze Song', duration: 1345}, {name:'Lollipop', duration: 1236}, {name:'Trolls Dance', duration: 1405}]
@@ -21,10 +17,6 @@ let fakeServerData = {
       {
         name: 'Christmas Mix',
         songs: [{name:'Jingle Bells', duration: 1345}, {name:'O Holy Night', duration: 1236}, {name:'All I Want For Christmas', duration: 1405}]
-      },
-      {
-        name: 'Workout Mix',
-        songs: [{name:'Workout Song 1', duration: 1345}, {name:'Workout Song 2', duration: 1236}, {name:'Workout Song 3', duration: 1405}]
       }
     ]
   }
@@ -74,7 +66,7 @@ class Playlist extends Component {
     let playlist = this.props.playlist
     return (
       <div style={{...defaultStyle, display: 'inline-block', width: "25%"}}>
-        <img/>
+        <img src={playlist.imageUrl} style={{width: '60px'}}/>
         <h3>{playlist.name} </h3>
         <ul>
           {playlist.songs.map(song =>
@@ -95,35 +87,66 @@ class App extends Component {
     }
   }
   componentDidMount() {
-    setTimeout(() => { //setTimeout is used to simulate how react would act. using arrow function
-    this.setState({serverData: fakeServerData});
-    }, 1000);
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed.access_token;
+    if (!accessToken)
+      return;
+    fetch('https://api.spotify.com/v1/me', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(data => this.setState({     //=> console.log(data)
+      user: { 
+        name: data.display_name
+      }
+    }))
+
+    fetch('https://api.spotify.com/v1/me/playlists', {
+      headers: {'Authorization': 'Bearer ' + accessToken}
+    }).then(response => response.json())
+    .then(data => this.setState({
+      playlists: data.items.map(item => {
+        console.log(data.items)
+        return {
+          name: item.name,
+          imageUrl: item.images[0].url, 
+          songs: []
+        }
+    })
+    }))
+
   }
   render() {
-    let playlistToRender = this.state.serverData.user ? this.state.serverData.user.playlists
-      .filter(playlist =>
-        playlist.name.toLowerCase().includes(
-          this.state.filterString.toLowerCase())
-    ) : []
-    let name = 'Jon'
-    let color = '#FF1212'
-    let headerStyle = {color: color, 'font-size': '50px'}
+    let playlistToRender = 
+      this.state.user && 
+      this.state.playlists 
+        ? this.state.playlists.filter(playlist =>
+          playlist.name.toLowerCase().includes(
+            this.state.filterString.toLowerCase())) 
+        : []
+    // let name = 'Jon'
+    // let color = '#FF1212'
+    // let headerStyle = {color: color, 'font-size': '50px'}
     return (
       <div className="App">
-        {this.state.serverData.user ? //the question mark is a turnery operator to create a loading dialog
+        {this.state.user ? //the question mark is a turnery operator to create a loading dialog
         <div>
           <h1 style={{...defaultStyle, 'font-size': '54px'}}>
-            {this.state.serverData.user.name}'s Playlist
+            {this.state.user.name}'s Playlists
           </h1>
-          <PlaylistCounter playlists= {playlistToRender}/>
-          <HoursCounter playlists= {playlistToRender}/>
+          <PlaylistCounter playlists={playlistToRender}/>
+          <HoursCounter playlists={playlistToRender}/>
           <Filter onTextChange={text => {
-            this.setState({filterString: text})
-          }}/>
+              this.setState({filterString: text})
+            }}/>
           {playlistToRender.map(playlist =>  //Map - Using to transform one array to another object array
             <Playlist playlist={playlist}/> 
           )}
-        </div> : <h1 style={defaultStyle}>Loading...</h1>
+        </div> : <button onClick={() => {
+            window.location = window.location.href.includes('localhost') 
+              ? 'http://localhost:8888/login' 
+              : 'https://better-playlists-backend-jm.herokuapp.com/login' }
+          }
+          style={{padding: '20px', 'font-size': '50px', 'margin-top': '20px'}}>Sign in with Spotify</button>
         }
       </div>
     );
